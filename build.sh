764817d30acbb2a4916d8dc3df82e32f84892877
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2025-12-10 01:57:25 +0000

    refactor: Remove Node.js and npm installation steps from build script

--- build.sh
+++ build.sh
@@ -6,19 +6,6 @@ set -o errexit  # Exit on error
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
 
-echo "Installing Node.js and npm..."
-# Install Node.js 24.x
-curl -fsSL https://deb.nodesource.com/setup_24.x | bash -
-apt-get install -y nodejs
-
-echo "Verifying Node.js and npm installation..."
-node --version
-npm --version
-
-echo "Installing Tailwind CSS dependencies..."
-cd theme/static_src
-npm install
-cd ../..
 
 echo "Building Tailwind CSS..."
 python manage.py tailwind build
