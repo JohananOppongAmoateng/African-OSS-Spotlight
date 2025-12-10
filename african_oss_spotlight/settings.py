2e68533d93bd90f2b1498394faef8bd80a6973cc
Johanan Oppong Amoateng <88411614+JohananOppongAmoateng@users.noreply.github.com>
2025-12-10 02:34:30 +0000

    Update ALLOWED_HOSTS for production settings

--- african_oss_spotlight/settings.py
+++ african_oss_spotlight/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-_w&gwywy6il+my1nf+qo3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ["https://african-oss-spotlight.onrender.com"]
+ALLOWED_HOSTS = ["african-oss-spotlight.onrender.com"]
 
 
 
